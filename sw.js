f0d4decf3bffffd6204bd2b252778562e4a7715f
ansinlau <ansinlau@dhcp-86-134.eduroam.aalto.fi>
2019-03-05 10:04:49 +0200

    v1.0.1

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89a1276c81c0cf215335.js"
+    "url": "webpack-runtime-003fa275322c718e0c15.js"
   },
   {
     "url": "app-ac9cdd5f3b4b60777eb2.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5a7800bf5e6c61e772cb13a840050d1d"
+    "revision": "422934ae6d8986ccb2afd6b201c222c8"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
