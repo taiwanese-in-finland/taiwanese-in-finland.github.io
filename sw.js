ec2b8cf5a47efb43cd99d0e2bab9802931cf1150
ansinlau <ansinlau@ansinlaus-MacBook-Pro.local>
2019-03-18 18:41:28 +0200

    v1.0.3 footer, event page based on api sheety, color

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-90343c90a527be3d292d.js"
+    "url": "webpack-runtime-01c8684522bddb3af0a6.js"
   },
   {
     "url": "app-af33fb2afba1a908b10f.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e96b83bb167dd4af32cf5dc1d1e82552"
+    "revision": "63fc70aad8cdfe156c145fbe309b679d"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
